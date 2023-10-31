e2a7c3ce38b233bc920b6a06446db17394e0cc8f
Syed Hisham Akmal <hisham0502@gmail.com>
2023-11-01 00:48:00 +0530

    Converted stream cipher into CLI

--- backend/streamcipher.py
+++ backend/streamcipher.py
@@ -1,8 +1,11 @@
+import click
 import random
+from termcolor import colored
+import os
 
 def generate_keystream(secret_key, length):
-    random.seed(secret_key) 
-    keystream = [random.randint(0, 255) for _ in range(length)]  
+    random.seed(secret_key)
+    keystream = [random.randint(0, 255) for _ in range(length)]
     return bytes(keystream)
 
 def encrypt(plaintext, secret_key):
@@ -15,14 +18,31 @@ def decrypt(ciphertext, secret_key):
     plaintext = bytes(c ^ k for c, k in zip(ciphertext, keystream))
     return plaintext
 
+@click.command()
+def main():
+    os.system('cls' if os.name == 'nt' else 'clear')  
+
+    banner = r'''
+ ________  ___  ________  ___  ___  _______   ________          ________  ________  ________  _________   
+|\   ____\|\  \|\   __  \|\  \|\  \|\  ___ \ |\   __  \        |\   ____\|\   __  \|\   __  \|\___   ___\ 
+\ \  \___|\ \  \ \  \|\  \ \  \\\  \ \   __/|\ \  \|\  \       \ \  \___|\ \  \|\  \ \  \|\  \|___ \  \_| 
+ \ \  \    \ \  \ \   ____\ \   __  \ \  \_|/_\ \   _  _\       \ \  \    \ \   __  \ \   _  _\   \ \  \  
+  \ \  \____\ \  \ \  \___|\ \  \ \  \ \  \_|\ \ \  \\  \|       \ \  \____\ \  \ \  \ \  \\  \|   \ \  \ 
+   \ \_______\ \__\ \__\    \ \__\ \__\ \_______\ \__\\ _\        \ \_______\ \__\ \__\ \__\\ _\    \ \__\
+    \|_______|\|__|\|__|     \|__|\|__|\|_______|\|__|\|__|        \|_______|\|__|\|__|\|__|\|__|    \|__|
+    '''
+    
+    click.echo(colored(banner, 'cyan'))
+    click.echo('Cipher Cart - Secure Data Encryption/Decryption')
+    click.echo('---------------------------------------------')
+
+    secret_key = click.prompt('Enter Secret Key', default=str(random.getrandbits(64)), show_default=True)
+    plaintext = click.prompt('Enter Plain Text', default='Hello World!', show_default=True)
+
+    ciphertext = encrypt(plaintext.encode('utf-8'), secret_key)
+    
+    click.echo(f'Ciphertext: {colored(ciphertext.hex(), "green")}')
+
+
 if __name__ == '__main__':
-    secret_key = input("Enter secret key: ") 
-    plaintext = input("Enter plaintext: ").encode('utf-8')
-    # OR plaintext = bytes(input("Enter plaintext: "),encoding='utf-8')
-    
-    ciphertext = encrypt(plaintext, secret_key)
-    decrypted_text = decrypt(ciphertext, secret_key)
-    
-    print(f'Plaintext: {plaintext.decode("utf-8")}')
-    print(f'Ciphertext: {ciphertext.hex()}')
-    print(f'Decrypted Text: {decrypted_text.decode("utf-8")}')
+    main()
