8fdeddd17693e1c3c08aa225eeb4f776b8799fec
Syed Hisham Akmal <hisham0502@gmail.com>
2023-11-01 00:26:29 +0530

    app.py: created a flask api for encrypting and decrypting using steam cipher

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,51 @@
-from flask import Flask
- 
-# Flask constructor takes the name of 
-# current module (__name__) as argument.
+from flask import Flask, request, jsonify
+import random
+
 app = Flask(__name__)
- 
-# The route() function of the Flask class is a decorator, 
-# which tells the application which URL should call 
-# the associated function.
-@app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-    return 'Hello World'
- 
-# main driver function
+
+def generate_keystream(secret_key, length):
+    random.seed(secret_key)
+    keystream = [random.randint(0, 255) for _ in range(length)]
+    return bytes(keystream)
+
+def encrypt(plaintext, secret_key):
+    keystream = generate_keystream(secret_key, len(plaintext))
+    ciphertext = bytes(p ^ k for p, k in zip(plaintext, keystream))
+    return ciphertext
+
+def decrypt(ciphertext, secret_key):
+    keystream = generate_keystream(secret_key, len(ciphertext))
+    plaintext = bytes(c ^ k for c, k in zip(ciphertext, keystream))
+    return plaintext
+
+@app.route('/encrypt', methods=['POST'])
+def encrypt_endpoint():
+    data = request.get_json()
+    secret_key = data.get('secret_key', "")
+    plaintext = data.get('plaintext', "")
+
+    if not secret_key or not plaintext:
+        return jsonify({'error': 'Both secret_key and plaintext are required.'}), 400
+
+    ciphertext = encrypt(plaintext.encode('utf-8'), secret_key)
+    return jsonify({'ciphertext': ciphertext.hex()})
+
+@app.route('/decrypt', methods=['POST'])
+def decrypt_endpoint():
+    data = request.get_json()
+    secret_key = data.get('secret_key', "")
+    ciphertext = data.get('ciphertext', "")
+
+    if not secret_key or not ciphertext:
+        return jsonify({'error': 'Both secret_key and ciphertext are required.'}), 400
+
+    try:
+        ciphertext_bytes = bytes.fromhex(ciphertext)
+    except ValueError:
+        return jsonify({'error': 'Invalid hex-encoded ciphertext.'}), 400
+
+    decrypted_text = decrypt(ciphertext_bytes, secret_key)
+    return jsonify({'decrypted_text': decrypted_text.decode('utf-8')})
+
 if __name__ == '__main__':
- 
-    # run() method of Flask class runs the application 
-    # on the local development server.
     app.run(debug=True)
