37e016cc1bc5ba7f287a7251773003a508a30c63
Syed Hisham Akmal <hisham0502@gmail.com>
2023-11-01 18:19:53 +0530

    Integrated block cipher APIs into app.py

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,8 @@ import random
 
 app = Flask(__name__)
 
+
+# Stream cipher methods
 def stream_generate_keystream(secret_key, length):
     random.seed(secret_key)
     keystream = [random.randint(0, 255) for _ in range(length)]
@@ -18,6 +20,48 @@ def stream_decrypt(ciphertext, secret_key):
     plaintext = bytes(c ^ k for c, k in zip(ciphertext, keystream))
     return plaintext
 
+# Block cipher methods
+def pad(plaintext, block_size):
+    padding = block_size - len(plaintext) % block_size
+    return plaintext + bytes([padding] * padding)
+
+def unpad(padded_text, block_size):
+    padding = padded_text[-1]
+    if padding > 0 and padding <= block_size:
+        return padded_text[:-padding]
+    else:
+        return padded_text
+
+def generate_key(block_size):
+    return bytes([random.randint(0, 255) for _ in range(block_size)])
+
+def encrypt_block(block, key):
+    return bytes(p ^ k for p, k in zip(block, key))
+
+def decrypt_block(ciphertext_block, key):
+    return bytes(c ^ k for c, k in zip(ciphertext_block, key))
+
+def encrypt(plaintext, key, block_size):
+    plaintext = pad(plaintext)
+    ciphertext = b''
+    for i in range(0, len(plaintext), block_size):
+        block = plaintext[i:i + block_size]
+        ciphertext_block = encrypt_block(block, key)
+        ciphertext += ciphertext_block
+
+    return ciphertext
+
+def decrypt(ciphertext, key, block_size):
+    plaintext = b''
+
+    for i in range(0, len(ciphertext), block_size):
+        ciphertext_block = ciphertext[i:i + block_size]
+        block = decrypt_block(ciphertext_block, key)    
+        plaintext += block
+
+    return unpad(plaintext)
+
+# Stream cipher endpoints
 @app.route('/stream-cipher/encrypt', methods=['POST'])
 def stream_encrypt_endpoint():
     data = request.get_json()
@@ -46,6 +90,39 @@ def decrypt_endpoint():
 
     decrypted_text = stream_decrypt(ciphertext_bytes, secret_key)
     return jsonify({'decrypted_text': decrypted_text.decode('utf-8')})
+
+
+# Block cipher endpoints
+
+@app.route('/block-cipher/encrypt', methods=['POST'])
+def encrypt_api():
+    data = request.get_json()
+    plaintext = data['plaintext']
+    block_size=data['block_size']
+
+    key = generate_key(block_size)
+    ciphertext = encrypt(plaintext.encode('utf-8'), key, block_size)
+
+    return jsonify({
+        'key': key.hex(),
+        'ciphertext': ciphertext.hex()
+    })
+
+@app.route('/block-cipher/decrypt', methods=['POST'])
+def decrypt_api():
+    data = request.get_json()
+    ciphertext_hex = data['ciphertext']
+    key_hex = data['key']
+    block_size = data['block_size']
+
+    key = bytes.fromhex(key_hex)
+    ciphertext = bytes.fromhex(ciphertext_hex)
+    decrypted_text = decrypt(ciphertext, key, block_size)
+
+    return jsonify({
+        'decrypted_text': decrypted_text.decode('utf-8')
+    })
+
 
 if __name__ == '__main__':
     app.run(debug=True)
