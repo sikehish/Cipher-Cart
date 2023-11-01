058e54eb38ee142236a0b0f64d34c5f25cef5bca
Syed Hisham Akmal <hisham0502@gmail.com>
2023-11-01 12:46:15 +0530

    Some modifications made in blockcipher.py

--- backend/blockcipher.py
+++ backend/blockcipher.py
@@ -3,8 +3,6 @@ import random
 from termcolor import colored
 import os
 
-BLOCK_SIZE = 16  # block size in bytes
-
 def pad(plaintext):
     # Pad the plaintext to be a multiple of BLOCK_SIZE
     padding = BLOCK_SIZE - len(plaintext) % BLOCK_SIZE
@@ -18,6 +16,10 @@ def unpad(padded_text):
     else:
         return padded_text
 
+def generate_key(key_size):
+    # Generate a random key of the specified size
+    return bytes([random.randint(0, 255) for _ in range(key_size)])
+
 def encrypt_block(block, key):
     return bytes(p ^ k for p, k in zip(block, key))
 
@@ -26,7 +28,7 @@ def decrypt_block(ciphertext_block, key):
 
 def encrypt(plaintext, key):
     plaintext = pad(plaintext)
-    ciphertext = b''
+    ciphertext = b'' # empty byte string at the start
 
     for i in range(0, len(plaintext), BLOCK_SIZE):
         block = plaintext[i:i + BLOCK_SIZE]
@@ -46,7 +48,8 @@ def decrypt(ciphertext, key):
     return unpad(plaintext)
 
 def faded_default_text(text):
-    return f"\033[90m{text}\033[0m" 
+    return f"\033[90m{text}\033[0m"
+
 @click.command()
 def main():
     os.system('cls' if os.name == 'nt' else 'clear')  
@@ -65,13 +68,17 @@ def main():
     click.echo('Cipher Cart - Secure Data using Block Cipher')
     click.echo('---------------------------------------------')
 
-    key = click.prompt('Enter Secret Key', default=faded_default_text('RandomKey' + str(random.getrandbits(64))))
+    # Get the block size from the user
+    global BLOCK_SIZE
+    BLOCK_SIZE = click.prompt('Enter Block Size', type=int, default=16)
+
+    # Generate a key of the specified block size
+    key = generate_key(BLOCK_SIZE)
     plaintext = click.prompt('Enter Plain Text', default=faded_default_text('Hello World!'))
 
     ciphertext = encrypt(plaintext.encode('utf-8'), key)
     decrypted_text = decrypt(ciphertext, key)
 
-    
     click.echo(f'Ciphertext: {colored(ciphertext.hex(), "green")}')
     click.echo(f'Decrypted Text: {colored(decrypted_text.decode("utf-8"), "green")}')
     click.echo('---------------------------------------------')
